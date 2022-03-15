0d8c84d3ce619dea64d3d7c924180e26b114ebf7
UnoWu <st9007a@gmail.com>
2022-03-15 09:38:33 +0800

    Fix bit mask

--- trie_opt.c
+++ trie_opt.c
@@ -6,8 +6,8 @@
 
 #define BLOCK_SIZE (256 * sizeof(void*))
 #define GET_CHILDREN(node) ((TrieNode**)((node)->payload & (0xffffffffffffffff << 2)))
-#define IS_END(node) ((node)->payload & 0x01)
-#define NEED_FREE(node) ((node)->payload & 0x10)
+#define IS_END(node) ((node)->payload & 1)
+#define NEED_FREE(node) ((node)->payload & 2)
 
 
 struct TrieNode {
@@ -15,21 +15,7 @@ struct TrieNode {
 };
 
 
-static TrieNode* trie_init_with_pool(void* pool, size_t pool_size) {
-    TrieNode* node;
-
-    if (pool_size > 0 && pool) {
-        node = pool;
-        memset(pool, 0, BLOCK_SIZE);
-        node->payload = (unsigned long)(pool + sizeof(TrieNode));
-    } else {
-        node = trie_init();
-    }
-
-    return node;
-}
-
-static inline void trie_add_with_pool(TrieNode* root, char* prefix, size_t length, void* pool, size_t pool_size) {
+static inline void trie_add_with_pool(TrieNode* root, char* prefix, size_t length, void* pool) {
     if (!length) {
         root->payload |= 1;
         return;
@@ -38,20 +24,20 @@ static inline void trie_add_with_pool(TrieNode* root, char* prefix, size_t lengt
     TrieNode* cur = GET_CHILDREN(root)[(*prefix) - 1];
 
     if (!cur) {
-        if (!pool || !pool_size) {
+        if (!pool) {
             pool = malloc(BLOCK_SIZE * length);
-            pool_size = length;
         }
 
-        cur = trie_init_with_pool(pool, pool_size);
+        memset(pool, 0, BLOCK_SIZE);
+        cur = pool;
+        cur->payload = (unsigned long)(pool + sizeof(TrieNode));
 
         GET_CHILDREN(root)[(*prefix) - 1] = cur;
 
         pool += BLOCK_SIZE;
-        --pool_size;
     }
 
-    trie_add_with_pool(cur, prefix + 1, length - 1, pool, pool_size);
+    trie_add_with_pool(cur, prefix + 1, length - 1, pool);
 
 }
 
@@ -62,13 +48,13 @@ size_t trie_struct_size() { return sizeof(TrieNode); }
 TrieNode* trie_init() {
     TrieNode* node = malloc(BLOCK_SIZE);
     memset(node, 0, BLOCK_SIZE);
-    node->payload = (unsigned long)(node + 1) | 0x10;
+    node->payload = (unsigned long)(node + 1) | 2;
 
     return node;
 }
 
 void trie_add(TrieNode* root, char* prefix, size_t length) {
-    trie_add_with_pool(root, prefix, length, NULL, 0);
+    trie_add_with_pool(root, prefix, length, NULL);
 }
 
 
@@ -90,15 +76,12 @@ void trie_free(TrieNode* node) {
     if (!node) {
         return;
     }
-    printf("Enter node:\n");
     for (int i = 0; i < 255; ++i) {
         if (GET_CHILDREN(node)[i]) {
-            printf("Access %c\n", i);
             trie_free(GET_CHILDREN(node)[i]);
         }
     }
     if (NEED_FREE(node)) {
-        printf("Free\n");
         free(node);
     }
 }
